0ce374ef5f4e34fba193699bf53d3b3cd1ecaba3
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-20 09:49:07 +0900

    update

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./TodoItem.css"
-const TodoItem = ({todo ,onDelete}) => {
+const TodoItem = ({todo ,onDelete,onUpdateChecked,onUpdateText}) => {
+  
+  const [editing, setEditing]=useState(false)
+  const [text, setText]=useState(todo.text)
+  const isCompleted = !!todo.isCompleted
+
+  const startEdit=()=>{
+    setText(todo.text)
+    setEditing(true)
+  }
+  const cancleEdit=()=>{
+    setText(todo.text)
+    setEditing(false)
+  }
+
+  const saveEdit =async()=>{
+    const next = text.trim()
+    
+    await onUpdateText(todo._id,next)
+  }
   return (
-    <div className='TodoItem'>
-        <input type="checkbox" readOnly/>
+    <div className={`TodoItem ${isCompleted? 'isCompleted':''}`}>
+        <input 
+        type="checkbox"
+        checked={todo.isCompleted} 
+        onChange={()=>onUpdateChecked(todo._id,!todo.isCompleted)}
+        readOnly />
         <div className="content">{todo.text}</div>
         <div className="date">{new Date(`${todo.date}`).toLocaleDateString()}</div>
         <div className="btn-wrap">
