541201b2cb79359c81a74f95b2ae2bcb28c81794
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-21 11:18:56 +0900

    update

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose=require("mongoose")
 const dotenv =require("dotenv")
 const cors=require("cors")
+const cookieParser = require("cookie-parser");
 
 dotenv.config()
 
@@ -13,6 +14,7 @@ app.use(cors({
     origin:process.env.FRONT_ORIGIN,
     credentials:true
 }))
+app.use(cookieParser())
 
 mongoose
     .connect(process.env.MONGO_URI)
@@ -21,9 +23,14 @@ mongoose
 
 
 
+
+
 const todoRoutes = require('./routes/todoRoutes')
 app.use('/api/todos',todoRoutes)
 
+
+const authRoutes = require('./routes/authRoutes')
+app.use('/api/auth',authRoutes)
 
 
 app.get('/',(req, res)=>{
